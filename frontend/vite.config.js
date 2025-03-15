b31000195b69781be3a3520d97b492f139afb9f5
Md Araf Husain <77201748+araf7053@users.noreply.github.com>
2025-03-15 08:33:33 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy :{
-      '/api' : 'http://localhost:4000',
+      '/api' : "https://screening-task-backend-ivcq.onrender.com/api/greet",
     },
 },
   plugins: [react()],
